345d69f14fe2b1e329fc31e25555ed3fb959009f
DESKTOP-B13MJUD\first <Nicole.Chang@axiomtek.com.tw>
2023-12-11 14:33:35 +0800

    fix: handle negative index in retryApi

--- src/redux/helper/reduxHelper.js
+++ src/redux/helper/reduxHelper.js
@@ -17,41 +17,51 @@ const retryApi = async ({
     const index = getState().reboot.serverList.findIndex(
       (s) => s.ip === server.ip
     );
-    const newToken = getState().reboot.serverList[index].token;
 
-    const axiosInstance = axios.create({
-      baseURL: "https://" + getState().reboot.backendIp,
-      timeout: 8000,
-    });
-    const serverObj = {
-      username: server.username,
-      password: server.password,
-      ip: server.ip,
-      token: newToken,
-    };
-    const body = { ...serverObj, ...params };
-    let axiosObj;
+    if (index < 0) {
+      retrySuccess = false;
+      retryResult = {
+        ip: server.ip,
+        response: "Failed to get token of this IP.",
+      };
+    } else {
+      const newToken = getState().reboot.serverList[index].token;
 
-    if (method === "POST") {
-      axiosObj = axiosInstance.post(api, body);
-    } else if (method === "GET") {
-      axiosObj = axiosInstance.get(api, body);
-    }
-
-    await axiosObj
-      .then((response) => {
-        retrySuccess = true;
-        retryResult = { ip: server.ip, response: response };
-        retryResult = { ...retryResult, ...params };
-      })
-      .catch((err) => {
-        console.error(err);
-        retrySuccess = false;
-        retryResult = {
-          ip: server.ip,
-          response: err.response ? err.response : err.code,
-        };
+      const axiosInstance = axios.create({
+        baseURL: "https://" + getState().reboot.backendIp,
+        timeout: 8000,
       });
+
+      const serverObj = {
+        username: server.username,
+        password: server.password,
+        ip: server.ip,
+        token: newToken,
+      };
+      const body = { ...serverObj, ...params };
+      let axiosObj;
+
+      if (method === "POST") {
+        axiosObj = axiosInstance.post(api, body);
+      } else if (method === "GET") {
+        axiosObj = axiosInstance.get(api, body);
+      }
+
+      await axiosObj
+        .then((response) => {
+          retrySuccess = true;
+          retryResult = { ip: server.ip, response: response };
+          retryResult = { ...retryResult, ...params };
+        })
+        .catch((err) => {
+          console.error(err);
+          retrySuccess = false;
+          retryResult = {
+            ip: server.ip,
+            response: err.response ? err.response : err.code,
+          };
+        });
+    }
   });
 
   return { retrySuccess: retrySuccess, retryResult: retryResult };
